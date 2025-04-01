4dd322da2df5cfb248bfd85e954c6c6bb699c4dd
William Freitas <freitas.esw@gmail.com>
2025-04-01 17:50:39 +0200

    add main functions

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='wfpy',
+    version='0.0.1',
+    description='Package with several tools used to analyse data from QMC methods.',
+    license='MIT',
+    packages=['wfpy'],
+    author='William Freitas',
+    keywords=['Quantum Monte Carlo, Graphics, Plots, Data analysis'],
+    url='https://github.com/freitas-esw/wfpy'
+)
